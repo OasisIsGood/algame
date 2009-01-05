967ac48c1b5e7a5dc7dc2f88c779b9ecc6a2e96a
gael.walter <gael.walter@2b811558-cb9a-11dd-8829-37334faa441c>
2009-01-05 15:31:13 +0000

    Link a son sprite...haut bas droite gauche, mais sans arme encore faudra un autre sprite pour ca

--- trunk/src/zelda/entity/characters/Link.java
+++ trunk/src/zelda/entity/characters/Link.java
@@ -13,7 +13,7 @@ import gameframework.game.GameMovable;
 
 public class Link extends GameMovable implements Drawable, GameEntity, Overlappable {
 
-	public static final int SPRITE_SIZE = 16;
+	public static final int SPRITE_SIZE = 24; // et pas 26 ou 16 ou autre! 24 !
 	protected static DrawableImage image = null;
 	protected Canvas defaultCanvas;
 	protected int spriteNumber = 0;
@@ -22,7 +22,7 @@ public class Link extends GameMovable implements Drawable, GameEntity, Overlappa
 	
 	public Link(Canvas defaultCanvas) {
 		if (image == null) {
-			image = new DrawableImage("images/characters/pac1.gif", defaultCanvas);
+			image = new DrawableImage("images/characters/zeldaa.gif", defaultCanvas);
 		}
 	}
 	
@@ -31,25 +31,34 @@ public class Link extends GameMovable implements Drawable, GameEntity, Overlappa
 		Point tmp = getSpeedVector().getDir();
 		movable = true;
 		if (tmp.getX() == 1) {
-			spriteType = 4;
+			System.out.println("DROITE");
+			spriteType = 0;
 		} else if (tmp.getX() == -1) {
-			spriteType = 5;
-		} else if (tmp.getY() == 1) {
-			spriteType = 7;
+			System.out.println("GAUCHE");
+			spriteType = 1;
 		} else if (tmp.getY() == -1) {
-			spriteType = 6;
+			System.out.println("HAUT");
+			spriteType = 2;
+		} else if (tmp.getY() == 1) {
+			System.out.println("BAS");
+			spriteType = 3;
 		} else {
-			spriteType = 9;
+			//System.out.println("DE FACE");
+			spriteType = 3;
 			spriteNumber = 0;
 			movable = false;
 		}
 		
-		g.drawImage(image.getImage(), (int) getPosition().getX(),
-				(int) getPosition().getY(), (int) getPosition().getX()
-						+ SPRITE_SIZE,
-				(int) getPosition().getY() + SPRITE_SIZE, spriteNumber * 32,
-				spriteType * 32, (spriteNumber + 1) * 32,
-				(spriteType + 1) * 32, null);
+		g.drawImage(image.getImage(), 
+				(int) getPosition().getX(),
+				(int) getPosition().getY(), 
+				(int) getPosition().getX() + SPRITE_SIZE, // ++ ou -- selon agrandissement ou retrecissement
+				(int) getPosition().getY() + SPRITE_SIZE, // ici aussi
+				spriteNumber * SPRITE_SIZE * 2,
+				spriteType * SPRITE_SIZE * 2, 
+				(spriteNumber + 1) * SPRITE_SIZE * 2,
+				(spriteType + 1) * SPRITE_SIZE * 2, 
+				null);
 	}
 
 	@Override
@@ -61,8 +70,7 @@ public class Link extends GameMovable implements Drawable, GameEntity, Overlappa
 	public void oneStepMoveHandler() {
 		if (movable) {
 			spriteNumber++;
-			spriteNumber = spriteNumber % 6;
+			spriteNumber = spriteNumber % 3;
 		}
 	}
-
 }
