142d9edbd188c3a8c0bbd8db29f40569f17d94f9
Gael.Walter <Gael.Walter@2b811558-cb9a-11dd-8829-37334faa441c>
2009-01-16 15:26:25 +0000

    improve sprites

--- trunk/src/zelda/entity/characters/SwordedLink.java
+++ trunk/src/zelda/entity/characters/SwordedLink.java
@@ -2,11 +2,17 @@ package zelda.entity.characters;
 
 import java.awt.Canvas;
 
+import zelda.entity.DrawableImageSprite;
+
 public class SwordedLink extends AbstractLink {
 	private static int STRENGH = 5;
 	
 	public SwordedLink(Canvas defaultCanvas) {
 		super(defaultCanvas);
+		if (image == null)
+			image	  = new DrawableImageSprite("images/characters/LINKSWORD.gif", defaultCanvas, 35, 35, 8);
+		spriteNumber = 0;
+		spriteType = 0;
 	}
 
 	@Override
